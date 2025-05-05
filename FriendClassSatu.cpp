d8387430a602b5d2fe105456697701bd09254291
rossakyl <rossakayla01@gmail.com>
2025-05-05 19:56:04 +0700

    Akses Data Privat dengan Friend Function

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -27,3 +27,22 @@ void orang::setNama(string pNama)
 {
     nama = pNama;
 }
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
+void siswa::displayAll(orang&a)
+{
+    cout << id << endl
+        << a.nama;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
