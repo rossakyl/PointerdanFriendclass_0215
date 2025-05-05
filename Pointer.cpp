a6617d7fece32a106c8e736927681c2d8430284d
rossakyl <rossakayla01@gmail.com>
2025-05-05 18:41:10 +0700

    Akses Nilai dan Alamat dengan Reference

--- Pointer.cpp
+++ Pointer.cpp
@@ -15,12 +15,11 @@ int main()
     cout << "Nilai dari C : " << *c << endl;
 
     // reference
+    int&d = b;
+    cout << "Nilai " << d << endl;
+    cout << "reference " << &d << endl;
 
-
-
-
-
-
-
+    int f = 5;
+    int g = f;
 
 }
